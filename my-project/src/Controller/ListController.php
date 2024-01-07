bd667ecf0b6c0a853f24073d6531ee912f6e6762
Nando PL <nandopiless@gmail.com>
2024-01-07 17:45:23 +0100

    Crud App finished

---
+++ my-project/src/Controller/ListController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Client;
+
+
+class ListController extends AbstractController
+{
+    /**
+     * Renders the template used for listing all the clients from the Clients table.
+     *
+     * @param  EntityManagerInterface $entityManager
+     * @return Response
+     */
+    #[Route('/client/list', name: 'list_clients')]
+    public function listClients(EntityManagerInterface $entityManager): Response
+    {
+        $clientRepository = $entityManager->getRepository(Client::class);
+        $clients = $clientRepository->findAll();
+
+        return $this->render('clientsList.html', [
+            'clients' => $clients,
+        ]);
+    }
+}
